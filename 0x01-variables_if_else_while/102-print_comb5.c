5e69931ddd0a85997ca26cd6e94e051d235ce740
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-18 17:01:38 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,24 +8,27 @@
 
 int main(void)
 {
-	int num1, num2;
+	int firstNo = 0, secondNo;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	while (firstNo <= 98)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		secondNo = firstNo + 1;
+		while ( secondNo <= 99)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
+			putchar((firstNo / 10) + '0');
+			putchar((firstNo % 10) + '0');
 			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
+			putchar((secondNo / 10) + '0');
+			putchar((secondNo % 10) + '0');
 
-			if (num1 == 98 && num2 == 99)
+			if (firstNo == 98 && secondNo == 99)
 				continue;
 
 			putchar(',');
 			putchar(' ');
+			secondNo++;
 		}
+		firstNo++;
 	}
 
 	putchar('\n');
