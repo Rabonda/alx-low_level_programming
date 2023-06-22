88d15f023012333c0d544fea4f08c8c2b1f0af20
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-22 08:52:43 +0200

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,19 +2,19 @@
 #include "main.h"
 
 /**
- * print_to_98 - print a sequence from a given number to 98
- * @n: strating number
- *
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: first number
+ * Void: displays all natural numbers
  */
 void print_to_98(int n)
 {
 	while (n != 98)
 	{
 		printf("%d", n);
-		if (n > 98)
-			--n;
-		else if (n < 98)
-			++n;
+		if (n < 98)
+			n + =1;
+		else if (n > 98)
+			n -= 1;
 		printf(", ");
 	}
 	printf("%d", 98);
