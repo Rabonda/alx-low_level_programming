ca30ab46ac2755738127b5549a150439a3c4e580
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-22 13:08:07 +0200

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
+* main - prints if integer is positive or negative
+* Return: 0 (code success)
 */
 
 int main(void)
 {
-int num;
+int i;
 
-num = 0;
-positive_or_negative(num);
+i = 0;
+positive_or_negative(i);
 
 return (0);
 }
