2a9b76236ad05dd411a6ec8fb47045fbea37e8ae
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-23 11:30:44 +0200

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,6 +4,7 @@
  * print_most_numbers - function that prints the numbers from 0 to 9
  * except 2 and 4
  * followed by a new line.
+ * @i - integer vriable
  * Return: void - prints numbers
  */
 void print_most_numbers(void)
