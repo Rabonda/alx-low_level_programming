6e6953dc8952736511d209acbbee519a8aee83a3
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-23 10:13:18 +0200

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
  * _isdigit - function that checks for a digit (0 through 9).
- * @c: char variable 
+ * @c: char variable
  * Return: 1 if c its between 0 and 9 or 0 if its not
  */
 int _isdigit(int c)
