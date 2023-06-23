acaa1636989ea95a4fdf383cd007df8040fcde0b
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-23 11:49:27 +0200

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,14 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	i = 0;
+	while (i <= 9)
 	{
 		if (i == 2 || i == 4)
 			continue;
 		_putchar('0' + i);
+
+		i++
 	}
 	_putchar('\n');
 }
