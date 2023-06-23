d0cf76176516a6e5433fe72c86ca95caef709069
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-23 15:45:35 +0200

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,22 +7,21 @@
  */
 void print_square(int size)
 {
-	int row, col;
+int row, col;
 
-	if (size > 0)
-	{
-		for (col = 0; col < size; col++)
-		{
-			for (row = 0; row < size; row++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
-	else
-	{
-		_putchar('\n');
-		
-	}
+if (size > 0)
+{
+for (col = 0; col < size; col++)
+{
+for (row = 0; row < size; row++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
