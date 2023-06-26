02fef53d782de10d576d47df8c491563fc5824e4
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-27 00:03:48 +0200

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,24 +1,28 @@
 #include "main.h"
 
 /**
- * puts_half - A function that prints half of a string.
- * @str: The string to be printed.
+ * puts_half -a function that prints half of a string
+ * followed by a new line.
+ * @str: The string variable to be printed in half.
+ * void: prints half of a string
  */
 void puts_half(char *str)
 {
-	int index = 0, len = 0, n;
+	int index = 0, half_number, strLen = 0;
 
 	while (str[index++])
-		len++;
+		strLen++;
 
-	if ((len % 2) == 0)
-		n = len / 2;
-
+	if (strLen % 2 != 0)
+		half_number = (strLen + 1) / 2;
 	else
-		n = (len + 1) / 2;
+		half_number = strLen / 2;
 
-	for (index = n; index < len; index++)
+	index = half_number;
+	while (index < strLen)
+	{
 		_putchar(str[index]);
-
+		index++
+	}
 	_putchar('\n');
 }
