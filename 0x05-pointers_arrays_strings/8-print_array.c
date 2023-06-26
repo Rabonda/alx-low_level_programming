f5f21140dde22941e6592efa2a586c81e05ba799
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-27 00:38:12 +0200

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,12 +9,13 @@ void print_array(int *a, int n)
 {
 	int index;
 
-	for (index = 0; index < n; index++)
+	index = 0;
+	while (index < n)
 	{
 		printf("%d", a[index]);
-
 		if (index != n - 1)
 			printf(", ");
+		index++;
 	}
 
 	printf("\n");
