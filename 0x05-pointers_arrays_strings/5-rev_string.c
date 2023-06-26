48e4b4b00d45df8b46b916a990c3eb683f819afe
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-26 22:53:15 +0200

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,19 +4,6 @@
  * rev_string - reverses a string
  * @s: string to reverse
  */
-/**int len = 0, index = 0;
-	char tmp;
-
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= len / 2; index--)
-	{
-		tmp = s[index];
-		s[index] = s[len - index - 1];
-		s[len - index - 1] = tmp;
-	}
-*/
 void rev_string(char *s)
 {
 	int i = 0;
