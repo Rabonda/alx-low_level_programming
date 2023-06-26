eae2909db2f035c335158189e15b10a622575248
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-26 19:56:48 +0200

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string to stdout.
- * @str: The string to be printed.
- * _putchar prints a new line
+ * _puts - a function that prints a string
+ * followed by a new line
+ * @str: The string vaiable.
+ * void: prints a string
  */
+
 void _puts(char *str)
 {
 	while (*str)
