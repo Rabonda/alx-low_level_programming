1dbd959ddb72571c8b85f897c653087c171ef296
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-28 19:13:07 +0200

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,8 +15,12 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index] && index < n; index++)
+	index = 0;
+	while (src[index] && index < n)
+	{
 		dest[dest_len++] = src[index];
+		index++;
+	}
 
 	return (dest);
 }
