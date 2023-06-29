6be315ca19aba4492ce79c18de5d9c4e7d122cdf
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-29 20:41:52 +0200

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,12 +9,12 @@ int main(void)
   a[2] = 1024;
   p = &n;
   /*
-   * You are not allowed to use the variable a in your new line of code
-   * You are not allowed to modify the variable p
-   * You can only write one statement
-   * You are not allowed to use ,
-   * You are not allowed to code anything else than the line of expected line
-   *Your code should be written at line 19, before the ;
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
    */
   *(p + 5) = 98;
   /* ...so that this prints 98\n */
