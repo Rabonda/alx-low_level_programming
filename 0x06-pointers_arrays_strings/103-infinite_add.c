084db5d68c9c87a2efce045d7cf633b1f075cb15
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-29 21:15:00 +0200

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,28 +14,28 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
  */
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
-	int num, tens = 0;
+	int number, tens = 0;
 
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
-		num = (*n1 - '0') + (*n2 - '0');
-		num += tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
+		number = (*n1 - '0') + (*n2 - '0');
+		number += tens;
+		*(r + r_index) = (number % 10) + '0';
+		tens = number / 10;
 	}
 
 	for (; *n1; n1--, r_index--)
 	{
-		num = (*n1 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
+		number = (*n1 - '0') + tens;
+		*(r + r_index) = (number % 10) + '0';
+		tens = number / 10;
 	}
 
 	for (; *n2; n2--, r_index--)
 	{
-		num = (*n2 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
+		number = (*n2 - '0') + tens;
+		*(r + r_index) = (number % 10) + '0';
+		tens = number / 10;
 	}
 
 	if (tens && r_index >= 0)
