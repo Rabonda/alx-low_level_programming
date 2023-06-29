a057640bf074bf454d9edf57d5d1cfa8cd65a7d7
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-29 21:54:45 +0200

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -46,30 +46,29 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 	return (r + r_index + 1);
 }
 /**
- * infinite_add - Adds two numbers.
+ * infinite_add - a function that adds two numbers.
  * @n1: The first number to be added.
  * @n2: The second number to be added.
- * @r: The buffer to store the result.
- * @size_r: The buffer size.
+ * @r: to store the result.
+ * @size_r: The size.
  *
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
+ * Return: store the sum 
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int index, n1_len = 0, n2_len = 0;
+	int num_index, len_n_1 = 0, len_n_2 = 0;
 
-	for (index = 0; *(n1 + index); index++)
-		n1_len++;
+	for (num_index = 0; *(n1 + num_index); num_index++)
+		len_n_1++;
 
-	for (index = 0; *(n2 + index); index++)
-		n2_len++;
+	for (num_index = 0; *(n2 + num_index); num_index++)
+		len_n_2++;
 
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+	if (size_r <= len_n_1 + 1 || size_r <= len_n_2 + 1)
 		return (0);
 
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
+	n1 += len_n_1 - 1;
+	n2 += len_n_2 - 1;
 	*(r + size_r) = '\0';
 
 	return (add_strings(n1, n2, r, --size_r));
