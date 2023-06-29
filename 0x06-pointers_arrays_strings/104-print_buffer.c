78b33dcc2c29de52938557ef4424e649535b1397
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-30 00:11:50 +0200

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,47 +10,38 @@
  */
 void print_buffer(char *b, int size)
 {
-	int byte_hex, byte_index;
-
-	byte_hex = 0;
-	while (byte_hex < size)
-	{
-		printf("%08x: ", byte_hex);
-
-		byte_index = 0;
-		while (byte_index <= 9)
-		{
-			if ((byte_index + byte_hex) >= size)
-				printf("  ");
-
-			else
-				printf("%02x", *(b + byte_index + byte_hex));
-
-			if ((byte_index % 2) != 0 && byte_index != 0)
-				printf(" ");
-			byte_index++;
-		}
-
-		byte_index = 0;
-		while (byte_index <= 9)
-		{
-			if ((byte_index + byte_hex) >= size)
-				break;
-
-			else if (*(b + byte_index + byte_hex) >= 31 &&
-				 *(b + byte_index + byte_hex) <= 126)
-				printf("%c", *(b + byte_index + byte_hex));
-
-			else
-				printf(".");
-			byte_index++;
-		}
-		if (byte_hex >= size)
-			continue;
-
-		printf("\n");
-		byte_hex += 10;
-	}
-	if (size <= 0)
-		printf("\n");
+int byte_hex, byte_index;
+byte_hex = 0;
+while (byte_hex < size)
+{
+printf("%08x: ", byte_hex);
+byte_index = 0;
+while (byte_index <= 9)
+{
+if ((byte_index + byte_hex) >= size)
+printf("  ");
+else
+printf("%02x", *(b + byte_index + byte_hex));
+if ((byte_index % 2) != 0 && byte_index != 0)
+printf(" ");
+byte_index++;
+}
+byte_index = 0;
+while (byte_index <= 9)
+{
+if ((byte_index + byte_hex) >= size)
+break;
+else if (*(b + byte_index + byte_hex) >= 31 && *(b + byte_index + byte_hex) <= 126)
+printf("%c", *(b + byte_index + byte_hex));
+else
+printf(".");
+byte_index++;
+}
+if (byte_hex >= size)
+continue;
+printf("\n");
+byte_hex += 10;
+}
+if (size <= 0)
+printf("\n");
 }
