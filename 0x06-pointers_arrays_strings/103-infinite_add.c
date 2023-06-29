9e6a6b7a74416027e194e58d82c5d5a9cd4dfb3b
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-29 22:03:07 +0200

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -56,19 +56,20 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int num_index, len_n_1 = 0, len_n_2 = 0;
+	int index, n1_len = 0, n2_len = 0;
 
-	for (num_index = 0; *(n1 + num_index); num_index++)
-		len_n_1++;
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
 
-	for (num_index = 0; *(n2 + num_index); num_index++)
-		len_n_2++;
+	for (index = 0; *(n2 + index); index++)
+		n2_len++;
 
-	if (size_r <= len_n_1 + 1 || size_r <= len_n_2 + 1)
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
 
-	n1 += len_n_1 - 1;
-	n2 += len_n_2 - 1;
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
 	*(r + size_r) = '\0';
+
 	return (add_strings(n1, n2, r, --size_r));
 }
