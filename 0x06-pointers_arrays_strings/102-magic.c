c3e502a2ae0e8f75b6b81f821ae89d38a5a491b6
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-29 20:24:43 +0200

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
-/**
- * main -  prints a[2] = 98, followed by a new line.
- * @a: array variable
- * @p: pointer variable
- * returns: 0 (code success)
- */
-
 int main(void)
 {
   int n;
@@ -15,8 +8,16 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
   *(p + 5) = 98;
+  /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
-  printf("\n")
   return (0);
 }
