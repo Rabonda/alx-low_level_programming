283496543e292975b4ec182f4f692daa121d6e50
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-29 22:11:57 +0200

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
-char *add_strings(char *n1, char *n2, char *r, int r_index);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
 /**
- * add_strings - Adds the numbers stored in two strings.
- * @n1: The string containing the first number to be added.
- * @n2: The string containing the second number to be added.
- * @r: The buffer to store the result.
- * @r_index: The current index of the buffer.
+ * add_strings - a function that adds the numbers stored in two strings.
+ * @n1: first number to be added.
+ * @n2: second number to be added.
+ * @r: store the result.
+ * @r_index: The index.
  *
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
+ * Return: store the sum
  */
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
