6edfc59a67f2f5fe8bbc033512b26b1ba3fe61d7
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-29 14:51:06 +0200

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,10 @@
 
 char *string_toupper(char *str)
 {
-	int index;
+	int str_index;
 
-	for (index = 0; str[index]; index++)
-		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] -= 32;
+	for (str_index = 0; str[str_index]; str_index++)
+		if (str[str_index] >= 'a' && str[str_index] <= 'z')
+			str[str_index] -= 32;
 	return (str);
 }
