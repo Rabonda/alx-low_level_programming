9ecbd7eeb7782e0af1034f8171989f9937624588
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-06-29 20:30:15 +0200

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - the program prints a[2] = 98, followed by a new line.
+ * @a: array variable
+ * @p: pointer variable
+ * @n: The number of array.
+ * Return: 0 (code success)
+ */
 int main(void)
 {
   int n;
@@ -8,6 +15,7 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
+
   *(p + 5) = 98;
   printf("a[2] = %d\n", a[2]);
   printf("\n")
