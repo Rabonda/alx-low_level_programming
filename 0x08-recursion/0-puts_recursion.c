62b711a8db37b06ab2956cd422765433f5180919
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-05 16:10:40 +0200

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string, followed by a new line.
- * @s: The string to be printed.
+ * _puts_recursion - a function that prints a string
+ * followed by a new line.
+ * @s: The string  variable to be printed.
  */
 
 void _puts_recursion(char *s)
