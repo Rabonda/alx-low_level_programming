fbd0d398e9b052ad18877e8ed0d479acdae11184
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-10 13:53:32 +0200

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - print name of program
- * @argc: argument counter
- * @argv: pointer to array of arguments
- * Return: 0 on success
+ * main - main function that print name of program
+ * @argc: argument counter.
+ * @argv: pointer to array of arguments.
+ * Return: 0 code success.
  */
 
 int main(int argc, char *argv[])
