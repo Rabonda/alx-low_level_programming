7bf1e4e45d39a37b7bbefe1cc9bad62bdd8e749e
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-10 14:51:39 +0200

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,23 +21,16 @@ int main(int argc, char *argv[])
 
 	while (count_cents > 0)
 	{
-		switch (count_cents)
-			{
-				case count_cents >= 25:
-					count_cents -= 25;
-				break;
-				case count_cents >= 10:
-					count_cents -= 10;
-				break;
-				case count_cents >= 5:
-					count_cents -= 5;
-				break;
-				case count_cents >= 2:
-					count_cents -= 2;
-				break;
-				case count_cents >= 1:
-					count_cents -= 1;
-			}
+		if (count_cents >= 25)
+			count_cents -= 25;
+		else if (count_cents >= 10)
+			count_cents -= 10;
+		else if (count_cents >= 5)
+			count_cents -= 5;
+		else if (count_cents >= 2)
+			count_cents -= 2;
+		else if (count_cents >= 1)
+			count_cents -= 1;
 		number_of_coins += 1;
 	}
 	printf("%d\n", number_of_coins);
