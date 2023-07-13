590bcd170f9cf1a9b0b69fd67a1ac53e866fbc18
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-13 14:04:46 +0200

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,26 +10,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
-	unsigned int x;
+	unsigned int i;
 
-	if (nmemb == 0)
-		return (NULL);
-	else if (size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	p = malloc(nmemb * size);
 
-	if (p != NULL)
-		continue;
-	else
+	if (p == NULL)
 		return (NULL);
 
-	x = 0;
-	while (i < (nmemb * size))
-	{
-		p[x] = 0;
-		x++;
-	}
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 
 	return (p);
 }
