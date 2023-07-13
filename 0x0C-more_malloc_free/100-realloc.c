98554e29fae844a5b4dbaffa4649bf96456fb877
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-13 15:11:33 +0200

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,13 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-/**
- * argstostr - function that reallocates a memory
- * block using malloc and free.
- * @ac: integer input variable.
- * @av: double pointer array.
- * Return: returns a memory block using malloc and free.
- */
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
