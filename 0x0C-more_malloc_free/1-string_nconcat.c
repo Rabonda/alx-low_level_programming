194ae23e5730345db6c722165feb9f0750878c1b
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-13 13:56:01 +0200

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		string_concat[string_len++] = s1[i];
 
 	for (i = 0; s2[i] && i < n; i++)
-		string_concat[len++] = s2[i];
+		string_concat[string_len++] = s2[i];
 
 	string_concat[string_len] = '\0';
 
