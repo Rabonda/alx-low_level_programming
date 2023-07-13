70baa0d447356984614fd812dfe21f68da720e60
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-13 14:53:54 +0200

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,35 +7,3 @@
  * @av: double pointer array.
  * Return: returns a memory block using malloc and free.
  */
-char *argstostr(int ac, char **av)
-{
-	int row, col, count = 0, len = 0;
-	char *arr_str;
-
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (row = 0; row < ac; row++)
-	{
-		for (col = 0; av[row][col]; col++)
-			row++;
-	}
-	len += ac;
-
-	arr_str = malloc(sizeof(char) * len + 1);
-	if (arr_str == NULL)
-		return (NULL);
-	for (row = 0; row < ac; row++)
-	{
-	for (col = 0; av[row][col]; col++)
-	{
-		arr_str[col] = av[row][col];
-		count++;
-	}
-	if (arr_str[count] == '\0')
-	{
-		arr_str[count++] = '\n';
-	}
-	}
-	return (arr_str);
-}
