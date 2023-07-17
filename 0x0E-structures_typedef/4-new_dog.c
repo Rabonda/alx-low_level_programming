6d16f60baad75c59ffefdb0fe4f28300f3bd71e8
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-17 23:25:46 +0200

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,74 +1,49 @@
 #include "dog.h"
-#include <stdio.h>
-/**
-*_strlen - function that returns length of a string.
-*@str: string variable to be counted.
-*Return: returns length of a string.
-*/
-int _strlen(char *str)
-{
-int len;
+#include <stdlib.h>
 
-for (len = 0; str; len++)
-continue;
-return (len);
-}
 /**
-*_strcopy - function that copies string pointed by src
-*into dest variable.
-*@dest:buffer storing.
-*@src: buffer storing string.
-*Return:returns copied string.
-*/
-char *_strcopy(char *dest, char *src)
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(char *str)
 {
-int index;
+	int length = 0;
+	char *ret;
 
-index = 0;
-while (src[index])
-{
-  dest[index] = src[index];
-  index++;
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
 }
-dest[index] = '\0';
-return (dest);
-}
+
 /**
-*new_dog - creates a new dog
-*@name: name of new dog
-*@age: age of new dog
-*@owner: owner of new dog
-*Return: returns NULL in case
-*of failure
-*/
+ * new_dog - instantiates a dawg
+ * @name: the dawgy name
+ * @age: the dawgy age
+ * @owner: the dawgy owner
+ *
+ * Return: pointer to new dawg.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *doggo;
+	dog_t *d = malloc(sizeof(dog_t));
 
-if (name == NULL || age < 0 || owner == NULL)
-  return (NULL);
-
-doggo = malloc(sizeof(dog_t));
-if (doggo == NULL)
-  return (NULL);
-
-doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-if (doggo->name == NULL)
-{
-  free(doggo);
-  return (NULL);
-}
-doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-if (doggo->owner == NULL)
-{
-  free(doggo->name);
-  free(doggo);
-  return (NULL);
-}
-
-doggo->name = _strcopy(doggo->name, name);
-doggo->age = age;
-doggo->owner = _strcopy(doggo->owner, owner);
-
-return (doggo);
+	if (!d)
+		return (NULL);
+	d->name = _strdup(name);
+	if (name && !d->name)
+		return (free(d), NULL);
+	d->owner = _strdup(owner);
+	if (owner && !d->owner)
+		return (free(d->name), free(d), NULL);
+	d->age = age;
+	return (d);
 }
