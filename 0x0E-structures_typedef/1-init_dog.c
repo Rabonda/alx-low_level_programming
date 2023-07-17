31a72498ea26886422b74015251a4f713631c7bc
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-17 22:49:56 +0200

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,13 +7,12 @@
 *@age: age of dog
 *@owner: dog owner's name
 */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-  if (d != NULL)
-  {
-    (*d).name = name;
-    (*d).age = age;
-    (*d).owner = owner;
-  }
+if (d != NULL)
+{
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
 }
