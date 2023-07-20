d63b366b2c7b2a173f82b25124b0c85bc9f2196d
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-20 22:15:57 +0200

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,47 +1,26 @@
-#include <stdio.h>
+#include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
-*main -  a program that prints the opcodes of its own main function.
-*@argc: number of arguments passed.
-*@argv: array of arguments.
-*
-*Return: on success, 1 or 2 in case of failure
-*/
-
-int main(int argc, char *argv[])
+ * main - check the code for Holberton School students.
+ * @argc: the number of args
+ * @argv: argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
 {
+	char *p = (char *)main;
+	int b;
 
-int i, b;
-int (*a)(int, char **) = main;
-unsigned char op_code;
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
 
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-
-b = atoi(argv[1]);
-
-if (b < 0)
-{
-printf("Error\n");
-exit(2);
-}
-
-i = 0;
-while (i < b)
-{
-op_code = *(unsigned char *)a;
-printf("%.2x", op_code);
-
-if (i == b - 1)
-continue;
-printf(" ");
-a++;
-i++;
-}
-printf("\n");
-return (0);
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
 }
