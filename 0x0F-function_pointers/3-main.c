a18fa0a0e68db079da5bfcd70643ab2ed4626b25
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-20 20:56:25 +0200

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 
 /**
-* main - Prints the result of simple operations.
-* @argc: The number of arguments supplied to the program.
+* main - function that prints the result of operations.
+* @argc: The number of arguments.
 * @argv: An array of pointers to the arguments.
 *
-* Return: Always 0.
+* Return: the result of operations.
 */
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-int num1, num2;
+int first_number, second_number;
 char *op;
 
 if (argc != 4)
@@ -21,9 +21,9 @@ printf("Error\n");
 exit(98);
 }
 
-num1 = atoi(argv[1]);
+first_number = atoi(argv[1]);
 op = argv[2];
-num2 = atoi(argv[3]);
+second_number = atoi(argv[3]);
 
 if (get_op_func(op) == NULL || op[1] != '\0')
 {
@@ -31,11 +31,11 @@ printf("Error\n");
 exit(99);
 }
 
-if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+if ((*op == '/' && second_number == 0) || (*op == '%' && second_number == 0))
 {
 printf("Error\n");
 exit(100);
 }
-printf("%d\n", get_op_func(op)(num1, num2));
+printf("%d\n", get_op_func(op)(first_number, second_number));
 return (0);
 }
