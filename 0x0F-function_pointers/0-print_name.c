ba7530ed3f8dd56282e93565dd1ba84d9888e362
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-20 07:15:22 +0200

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,15 @@
 #include "function_pointers.h"
 
 /**
-*print_name - prints a name
-*@name:name to print
+*print_name - a function that prints a name.
+*@name: variable to be printed.
 *@f: function which print name
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL)
-return;
-else if (f == NULL)
-return;
+if (name != NULL)
+f(name);
+else if (f != NULL)
 f(name);
 }
