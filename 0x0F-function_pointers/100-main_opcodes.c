7583c463e9c666aa8f6a000dbb07c121cf9398c5
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-20 22:48:00 +0200

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -13,15 +13,15 @@
 int main(int argc, char **argv)
 {
 	char *p = (char *)main;
+	int b;
 
 	if (argc != 2)
 		printf("Error\n"), exit(1);
-	else if (atoi(argv[1]) < 0)
+	b = atoi(argv[1]);
+	if (b < 0)
 		printf("Error\n"), exit(2);
 
-	while ((atoi(argv[1]))--)
-	{
+	while (b--)
 		printf("%02hhx%s", *p++, b ? " " : "\n");
-	}
 	return (0);
 }
