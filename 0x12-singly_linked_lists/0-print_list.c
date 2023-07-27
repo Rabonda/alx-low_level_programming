a8f33ff91290165cef89edd26f3ab6a36fca54b7
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-27 19:46:46 +0200

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * list_len - function that prints
+ * all the elements of a list_t list.
+ * @h - linked list.
+ * Return:Returns the number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t index;
+
+	index = 0;
+	while (h != NULL)
+	{
+	    if (h->str == NULL)
+	        printf("[0] (nil)\n");
+	    else
+	       printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		index += 1;
+	}
+	return (index);
+}
