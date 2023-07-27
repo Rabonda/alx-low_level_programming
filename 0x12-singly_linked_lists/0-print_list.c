e50a77879c8be626c4bf90793166a38ed0be934e
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-27 19:18:16 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,13 @@
 #include "lists.h"
+
 /**
-*print_list - function that prints all the elements of a list.
+*print_list - function that prints all
+*the elements of a list.
 *@h: list parameter.
 *
 *Return: returns the number of nodes.
 */
+
 size_t print_list(const list_t *h)
 {
 size_t index;
