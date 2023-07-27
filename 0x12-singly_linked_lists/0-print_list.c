c78364f338154d2e26409196118407c64110465f
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-27 19:22:57 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,30 +1,27 @@
 #include "lists.h"
-
 /**
-*print_list - function that prints all
-*the elements of a list.
-*@h: list parameter.
-*
-*Return: returns the number of nodes.
-*/
-
+ * print_list - function that prints all the elements of a list_t list.
+ *
+ * @h: Parameter with the list
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
-size_t index;
+	size_t i;
 
-index = 0;
-while (h != NULL)
-{
-if (h->str == NULL)
-{
-printf("[0] (nil)\n");
-}
-else
-{
-printf("[%u] %s\n", h->len, h->str);
-}
-h = h->next;
-index += 1;
-}
-return (index);
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+
+	return (i);
 }
