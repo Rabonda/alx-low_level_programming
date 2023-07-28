0fa0f6c640d4f150e0a80fc73af363ebc60894f0
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-28 14:10:27 +0200

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_list - Function that frees a list_t.
+* @head: The pointer.
+* Return: frees a list_t
+*/
+void free_list(list_t *head)
+{
+    list_t *temporary_location;
+
+    while (!head)
+    {
+        temporary_location = head;
+        head = head->next;
+        free(temporary_location->str);
+        free(temporary_location);
+    }
+    free(head);
+}
