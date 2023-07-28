138858fdb521d4d969754d30ae4eb8d46478dfbb
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-28 17:34:28 +0200

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-/**
-* bmain - function that prints You're beat! and yet
-* you must allow,\nI bore my house upon my back!\n
-* before the main function is executed.
-* Return: prints two statements.
-*/
 
-void __attribute__ ((constructor)) bmain()
+void first(void) __attribute__ ((constructor));
+
+/**
+  * first - function executed before main
+  *
+  * Return: prints two statements.
+  */
+void first(void)
 {
-printf("You're beat! and yet, you must allow");
-printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
