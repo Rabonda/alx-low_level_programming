0762f82d355ece5990f393d32c60c89167212465
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-28 08:41:48 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,20 +7,17 @@
 * @h: pointer of list.
 * Return: Returns the number of nodes.
 */
-size_t print_list(const list_t *h)
-{
+size_t print_list(const list_t *h) {
     size_t index;
 
     index = 0;
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
-	else
-	{
-		printf("[%u] %s\n", h->len, h->str);
+    while (h != NULL) {
+        if (h->str == NULL) {
+            printf("[0] (nil)\n");
+        }
+        else
+        {
+            printf("[%u] %s\n", h->len, h->str);
         }
         h = h->next;
         index++;
