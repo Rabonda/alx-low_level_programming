50c7e0cd231993fb99a9f90836dc17f770b4a8bc
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-28 10:11:27 +0200

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* list_len - a function that returns the number of elements
+* in a linked list_t list.
+* @h: A linked list.
+*
+*
+* Return:Returns the number of elements
+*/
+size_t list_len(const list_t *h)
+{
+    size_t index;
+
+    for (index = 0; h != NULL; index++)
+        h = h->next;
+
+    return index;
+}
