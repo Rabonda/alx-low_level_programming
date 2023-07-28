7e5a6f869fd57c97351b816f95d3029dea81dd24
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-28 14:24:58 +0200

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,12 +8,12 @@
 */
 void free_list(list_t *head)
 {
-    list_t *temporary_location;
+list_t *temporary_location;
 
-    while ((temporary_location = head) != NULL)
-    {
-        head = head->next;
-        free(temporary_location->str);
-        free(temporary_location);
-    }
+while ((temporary_location = head) != NULL)
+{
+head = head->next;
+free(temporary_location->str);
+free(temporary_location);
+}
 }
