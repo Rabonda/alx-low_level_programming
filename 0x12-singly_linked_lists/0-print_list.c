ec2cebbc6119b079a72ea3c5e427289b0339f4d5
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-07-28 08:26:55 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,14 @@ size_t print_list(const list_t *h)
     index = 0;
     while (h != NULL)
     {
-        printf("[%d] %s\n", h->len, h->str);
+        if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+            printf("[%d] %s\n", h->len, h->str);
+        }
         h = h->next;
         index++;
     }
