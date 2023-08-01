7661ccf3ab79c9ffc9c1c0c57465e870d300089b
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-01 14:17:01 +0200

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,6 +6,48 @@
   */
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	#include "lists.h"
+/**
+  * reverse_listint - a function that reverses a listint_t linked list.
+  * @head: describe argument.
+  * Return: reverses a listint_t linked list.
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *t, *p;
+
+	if (!head || !*head)
+		return (NULL);
+
+	if (!*head->next)
+		return (*head);
+
+	t = *head;
+	p = NULL;
+	while (1)
+	{
+		if (p == NULL)
+		{
+			p = *head;
+			t = t->next;
+			p->next = NULL;
+			*head = p;
+		}
+		else
+		{
+			p = t;
+			t = t->next;
+			p->next = *head;
+			if (t == NULL)
+			{
+				t = p;
+				break;
+			}
+			*head = p;
+		}
+	}
+	*head = t;
+
+	return (*head);
+
 }
