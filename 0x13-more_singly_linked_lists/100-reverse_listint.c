ff7a9caeae282fc00783c4d67ca2686fbc8057b2
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-01 14:19:49 +0200

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,40 +6,6 @@
   */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *t, *p;
-
-	if (!head || !*head)
-		return (NULL);
-
-	if (!*head->next)
-		return (*head);
-
-	t = *head;
-	p = NULL;
-	while (1)
-	{
-		if (p == NULL)
-		{
-			p = *head;
-			t = t->next;
-			p->next = NULL;
-			*head = p;
-		}
-		else
-		{
-			p = t;
-			t = t->next;
-			p->next = *head;
-			if (t == NULL)
-			{
-				t = p;
-				break;
-			}
-			*head = p;
-		}
-	}
-	*head = t;
-
+	head++;
 	return (*head);
-
 }
