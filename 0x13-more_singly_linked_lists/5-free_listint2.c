c9fe36d7b9f42029e739c05b442b6b2ac9e1e672
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-01 12:45:23 +0200

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
-  * free_listint2 -  a function that frees a listint_t list.
+  * free_listint2 - a function that frees a listint_t list.
   * @head: double pointer to head of list.
   * void: frees a listint_t list.
   */
@@ -15,11 +15,12 @@ void free_listint2(listint_t **head)
 	}
 
 	current_location = *head;
-	for (current_location != NULL)
+	while (current_location)
 	{
 		temporary = current_location;
-		cureent = current_location->next;
+		current_location = current_location->next;
 		free(temporary);
 	}
+
 	*head = NULL;
 }
