5e43ba031e5ee31666cc286ca0d11a075e903591
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-01 13:24:59 +0200

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,34 +13,27 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *node, *pre;
+	listint_t *current, *subsequent;
 
-	if (!(*head))
+	if (!head || !*head)
 		return (-1);
-	i = 0;
-	pre = NULL;
-	node = *head;
-
-	if (!index)
+	current = *head;
+	if (index == 0)
 	{
-		(*head) = (*head)->next;
-		free(node);
+		*head = (*head)->next;
+		free(current);
 		return (1);
 	}
-	while (node)
+	for (i = 0; i < (index - 1); i++)
 	{
-		if (i == index)
-		{
-			pre->next = node->next;
-			free(node);
-			return (1);
-		}
-		pre = node;
-		node = node->next;
-		i++;
+		current = current->next;
+		if (current == NULL)
+			return (-1);
 	}
-
-	return (-1);
+	subsequent = current->next;
+	current->next = subsequent->next;
+	free(subsequent);
+	return (1);
 
 }
 
