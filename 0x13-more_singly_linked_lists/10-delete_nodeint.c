54b17caa7752aeb90ab3d8fb27909f07b22c31f2
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-01 13:48:40 +0200

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,34 +12,33 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int count = 0;
-	listint_t *current_location = NULL, temporary = *head;
+unsigned int count = 0;
+listint_t *current_location = NULL, temporary = *head;
 
-	if (!*head)
-	{
-		return (-1);
-	}
+if (!*head)
+{
+return (-1);
+}
 
-	if (index == 0)
-	{
-		(*head) = (*head)->next;
-		free(node);
-		return (1);
-	}
+if (index == 0)
+{
+(*head) = (*head)->next;
+free(node);
+return (1);
+}
+while (count < index - 1)
+{
+if (!temporary || temporary->next)
+{
+return (-1);
+}
+temporary = temporary->next;
+count++;
+}
 
-	while (count < index - 1)
-	{
-		if (!temporary || temporary->next)
-		{
-			return (-1);
-		}
-		temporary = temporary->next;
-		count++;
-	}
+current_location = temporary->next;
+temporary->next = current_location->next;
+free(current_location);
 
-	current_location = temporary->next;
-	temporary->next = current_location->next;
-	free(current_location);
-
-	return (1);
+return (1);
 }
