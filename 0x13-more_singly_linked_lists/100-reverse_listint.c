4fa1be48257fbb568bda17e2709224f50fd263cf
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-01 14:12:55 +0200

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+  * reverse_listint - a function that reverses a listint_t linked list.
+  * @head: describe argument.
+  * Return: reverses a listint_t linked list.
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
