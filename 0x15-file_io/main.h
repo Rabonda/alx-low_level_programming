21ad6386a53701b157653dfbdcf625197f5a8c46
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-07 09:51:17 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -24,4 +24,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void from_to(copy_struct *cpy);
+void file_copy(copy_struct *cpy);
 #endif
