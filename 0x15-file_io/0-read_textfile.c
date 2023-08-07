634f155c6313d685b89b8e256ba302d7389801a4
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-07 08:38:09 +0200

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a text file and prints
+ * it to the POSIX standard output.
+ * @filename: relative or absolute path of the file.
+ * @letters: read and print the number of letters.
+ *
+ * Return: returns the POSIX standard output.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *toPrint;
+	ssize_t read_file, write_file;
+	int file_rd;
+
+	if (!filename)
+		return (0);
+
+	toPrint = malloc(sizeof(char) * letters);
+	if (!toPrint)
+		return (0);
+	file_rd = open(filename, O_RDONLY);
+	if (file_rd < 0)
+	{
+		free(toPrint);
+		return (0);
+	}
+	read_file = read(file_rd, toPrint, letters);
+	if (read_file < 0)
+	{
+		free(toPrint);
+		return (0);
+	}
+	if (file_rd > 0)
+		write_file = write(STDOUT_FILENO, toPrint, read_file);
+	if (write_file < read_file)
+	{
+		free(toPrint);
+		return (0);
+	}
+	write_file = close(file_rd);
+	if (write_file < 0)
+	{
+		free(toPrint);
+		return (0);
+	}
+	free(toPrint);
+	return (read_file);
+}
