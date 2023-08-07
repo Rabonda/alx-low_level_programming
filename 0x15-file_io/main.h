8d32fecf95f12076e6a765a15094d32b5b84dcd6
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-07 09:50:52 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -23,4 +23,5 @@ typedef struct copy_struct
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void from_to(copy_struct *cpy);
 #endif
