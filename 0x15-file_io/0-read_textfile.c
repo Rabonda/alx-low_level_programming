869244c830a6533eef51a906a198846a26a8b4c1
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-08 21:29:18 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,34 +13,34 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
-	ssize_t read_file, write_file;
-	char *bufer;
+int file;
+ssize_t read_file, write_file;
+char *bufer;
 
-	if (!filename)
-  {
-		return (0);
-  }
+if (!filename)
+{
+return (0);
+}
 
-	file = open(filename, O_RDONLY);
+file = open(filename, O_RDONLY);
 
-	if (file == -1)
-  {
-		return (0);
-  }
+if (file == -1)
+{
+return (0);
+}
 
-	bufer = malloc(sizeof(char) * (letters));
-	if (!bufer)
-  {
-		return (0);
-  }
+bufer = malloc(sizeof(char) * (letters));
+if (!bufer)
+{
+return (0);
+}
 
-	read_file = read(file, bufer, letters);
-	write_file = write(STDOUT_FILENO, bufer, read_file);
+read_file = read(file, bufer, letters);
+write_file = write(STDOUT_FILENO, bufer, read_file);
 
-	close(file);
+close(file);
 
-	free(bufer);
+free(bufer);
 
-	return (write_file);
+return (write_file);
 }
