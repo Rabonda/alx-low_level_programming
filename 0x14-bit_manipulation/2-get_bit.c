61a41862e3584373a1715e92224ecd88c977fae9
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-08-31 23:11:33 +0200

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,11 +2,11 @@
 #include "main.h"
 #include <math.h>
 /**
- * get_bit - a function that sets the value of a bit 
+ * get_bit - a function that sets the value of a bit
  * to 1 at a given index.
  * @index: is the index, starting from 0.
  * @n: bit value in the index.
- * Return: 1 if it worked, or -1 if an error occurred
+ * Return: 1 if it worked, or -1 if an error occurred.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
