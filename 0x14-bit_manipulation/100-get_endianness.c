ae5d0d230dd8d59bff285cbf50d088c25326c99b
Rabonda <68507290+Rabonda@users.noreply.github.com>
2023-09-01 11:50:28 +0200

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_endianness -  a function that checks the endianness.
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int  little_endian = 1;
+	char * big_endian = (char *)&little_endian;
+
+	if (*big_endian)
+		return (1);
+  else
+    return (0);
+
+}
