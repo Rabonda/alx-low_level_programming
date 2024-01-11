088ab667432f60140adddb8f39e3b2f462014f28
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-12 00:15:45 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,45 +2,45 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - function that insert node at nth index of double linked list
+ * insert_dnodeint_at_index - insert node at nth index of double linked list
  * @h: pointer to head
  * @idx: integer
  * @n: integer
- * Return: returns address of node inserted at the nth index or null if failed
+ * Return: address of node inserted at the nth index or null if failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_ptr, *t, *s;
+	dlistint_t *new, *temp, *store;
 	unsigned int i = 0;
 	unsigned int number_nodes = 0;
 
-	t = *h;
-	while (t)
+	temp = *h;
+	while (temp)
 	{
 		number_nodes += 1;
-		t = t->next;
+		temp = temp->next;
 	}
-	new_ptr = malloc(sizeof(dlistint_t));
-	if (new_ptr == NULL)
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	if (idx == number_nodes)
 		return (add_dnodeint_end(h, n));
-	new_ptr->n = n;
-	t = *h;
-	while (t)
+	new->n = n;
+	temp = *h;
+	while (temp)
 	{
 		if (i == idx - 1)
 		{
-			s = t->next;
-			t->next = new_ptr;
-			new_ptr->prev = t;
-			new_ptr->next = s;
-			s->prev = new_ptr;
-			return (new_ptr);
+			store = temp->next;
+			temp->next = new;
+			new->prev = temp;
+			new->next = store;
+			store->prev = new;
+			return (new);
 		}
-		t = t->next;
+		temp = temp->next;
 		i++;
 	}
 	return (NULL);
