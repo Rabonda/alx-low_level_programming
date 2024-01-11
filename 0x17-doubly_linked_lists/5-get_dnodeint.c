8729697149425f9b7c481be7321cef71ea66feee
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-11 23:42:57 +0200

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function that get node at nth index of double linked list
+ * @head: pointer to head
+ * @index: integer
+ * Return: returns a node at the nth index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int indx = 0;
+
+	for (head)
+	{
+		if (index == indx)
+			return (head);
+		head = head->next;
+		indx = indx + 1;
+	}
+	return (NULL);
+}
