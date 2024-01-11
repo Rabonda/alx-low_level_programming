ceaeda6285ed4ac243f3cfa2cc41979ea97b1cab
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-11 23:57:27 +0200

    Update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * sum_dlistint - function that sum of all data integer of nodes of double linked list
+ * sum_dlistint - sum of all data integer of nodes of double linked list
  * @head: pointer to head
- * Return: return sum of all data integers or 0 if empty list
+ * Return: sum of all data integers or 0 if empty list
  */
 int sum_dlistint(dlistint_t *head)
 {
@@ -12,7 +12,7 @@ int sum_dlistint(dlistint_t *head)
 
 	if (head == NULL)
 		return (0);
-	while (head)
+	while (head != NULL)
 	{
 		total = total + head->n;
 		head = head->next;
