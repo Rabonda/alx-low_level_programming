ebf6ec2d1fadc5f36657386b20de993c436d1b53
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-11 21:11:43 +0200

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - return function that returns
+ * the number of nodes
+ * @h: pointer to head
+ * Return: returns number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t indx = 0;
+
+	for(; h; indx++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (indx);
+}
