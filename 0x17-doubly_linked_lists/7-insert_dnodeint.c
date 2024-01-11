ffd0569f44d02ba523099a96ca0354f386aa02f8
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-12 00:14:48 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 	new_ptr->n = n;
 	t = *h;
-	for (; t;)
+	while (t)
 	{
 		if (i == idx - 1)
 		{
