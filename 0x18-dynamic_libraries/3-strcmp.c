3fc111db4cbce359e316e1956b37db9f04ea02b7
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-15 14:03:41 +0200

    strcmp

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings.
+ * @s1: first string to be compared.
+ * @s2: second string to be compared.
+ * Return: compared string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
