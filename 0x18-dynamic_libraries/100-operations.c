ebc878dbc5f1667f1511e83ed59e1796aca5513e
Rabonda <68507290+Rabonda@users.noreply.github.com>
2024-01-15 15:20:27 +0200

    100-operations.c

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,1 +1,32 @@
-100-operations.c
+#include <stdio.h>
+
+int add(int a, int b)
+{
+  return (a + b);
+}
+
+int sub(int a, int b)
+{
+  return (a - b);
+}
+
+int mul(int a, int b)
+{
+  return (a * b);
+}
+
+int div(int a, int b)
+{
+  if(b == 0)
+    return 0;
+  else
+    return (a / b);
+}
+
+int mod(int a, int b)
+{
+  if(b == 0)
+    return 0;
+  else
+    return (a % b);
+}
